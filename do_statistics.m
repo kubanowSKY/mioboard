f9d9d2272eb2af48f8e135906404dde5e0c51436
Patryk Kulpanowski <kuban@newterm.pl>
2016-11-19 13:35:28 +0100

    statistics: minor fixes for statistics
    + reworked statistics script for taking all probes from every sample to
    workspace
    + hardcoded average range on statistics

--- do_statistics.m
+++ do_statistics.m
@@ -1,8 +1,8 @@
-function [average, vary, std] = do_statistics(specter)
+function [average, vary, std_dev] = do_statistics(specter)
     for ii=1:2
-        average(ii)=mean(abs(specter(:,ii)));
-        vary(ii)=var(specter(:,ii));
-        std(ii)=sqrt(vary(ii));
+        average(ii)=mean(abs(specter(40:250,ii)));
+        vary(ii)=var(specter(40:250,ii));
+        std_dev(ii)=sqrt(vary(ii));
     end
 end
 

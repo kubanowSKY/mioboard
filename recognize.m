85e390e934a0aa852c3980d6efe19f1d1ad89963
Patryk Kulpanowski <kuban@newterm.pl>
2017-01-05 14:25:28 +0100

    recognize: fixed key status logic

--- recognize.m
+++ recognize.m
@@ -3,36 +3,36 @@ function [ gesture, state ] = recognize(probes, key, state, filters)
 	[sum_can1, sum_can2, av_ex] = average_diff(filtered_probes);
 %     gesture = 3
 	if (abs(sum_can1 - sum_can2) <= 10^-5)
-        gesture = 0; % recognize as open hand gesture
-        if state ~= 0
+        gesture = 1; % recognize as open hand gesture
+        if state ~= 1
             release_all(key);
             press_up(key);
-            state = 0;
+            state = 1;
         end
     
 
     elseif (sum_can1 < sum_can2) || (av_ex <= -0.5)
-        gesture = 1; % recognize as index gesture
-        if state ~= 1
+        gesture = 2; % recognize as index gesture
+        if state ~= 2
             release_all(key);
             press_right(key);
-            state = 1;
+            state = 2;
         end
     
     elseif (sum_can1 > sum_can2) || (av_ex >= 0.5)
-        gesture = 2; % recognize as hand close gesture
-        if state ~= 2
+        gesture = 0; % recognize as hand close gesture
+        if state ~= 0
             release_all(key);
             press_left(key);
-            state = 2;
+            state = 0;
         end
         
     else
-        gesture = 0; % recognize as hand open gesture
-        if state ~= 0
+        gesture = 1; % recognize as hand open gesture
+        if state ~= 1
             release_all(key);
             press_up(key);
-            state = 0;
+            state = 1;
         end
     end
 end
